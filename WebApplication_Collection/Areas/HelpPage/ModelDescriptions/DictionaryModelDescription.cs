959f30410bb4c43ed53a40cfd17088a42c477eaa
aaron_clark_aic <aaron_clark_aic@hotmail.com>
2015-09-25 15:17:07 +0800

    1.添加了gitignore文件 2.所有使用过的方法/类型/解决方案/思路的demo和测试

---
+++ WebApplication_Collection/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApplication_Collection.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebApplication_Collection/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace WebApplication_Collection.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
